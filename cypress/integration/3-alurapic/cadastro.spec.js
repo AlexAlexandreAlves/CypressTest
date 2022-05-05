c8cdb557c19986e71ec9adb34fb874f8540e1b2d
Alex Alenxandre Alves <alex.alexandre@ngi.com.br>
2022-05-04 21:20:42 -0300

    feat: utilizando array para efetuar inclusões

--- cypress/integration/3-alurapic/cadastro.spec.js
+++ cypress/integration/3-alurapic/cadastro.spec.js
@@ -4,16 +4,19 @@ describe('Cadastro de usuarios alura pic', () => {
         cy.visit('/')
 
     })
+    const usuarios = require('../../fixtures/usuarios.json');
+    usuarios.forEach(usuario => {
 
-    it('verifica mensagens validacao', () => {
-        cy.contains('a', 'Register now').click();
-        cy.contains('button', 'Register').click();
-        cy.contains('ap-vmessage', 'Email is required!').should('be.visible');
-        cy.contains('button', 'Register').click();
-        cy.contains('ap-vmessage', 'User name is required!').should('be.visible');
-        cy.contains('ap-vmessage', 'Password is required!').should('be.visible');
-        cy.contains('ap-vmessage', 'Full name is required!').should('be.visible');
-    })
+        it(`registra novo usuario ${usuario.userName} `, () => {
+            cy.contains('a', 'Register now').click();
+            cy.contains('button', 'Register').click();
+            cy.get('input[formcontrolname="email"]').type(usuario.email);
+            cy.get('input[formcontrolname="fullName"]').type(usuario.fullName);
+            cy.get('input[formcontrolname="userName"]').type(usuario.userName);
+            cy.get('input[formcontrolname="password"]').type(usuario.password);
+            cy.contains('button', 'Register').click();
+        })
+    });
 
     it('verifica mensagens de email invalido', () => {
         cy.contains('a', 'Register now').click();
